0bbea5e1ae2edeb076518db08a3a0dde0671348f
alakhpc <36898190+alakhpc@users.noreply.github.com>
2024-04-12 03:43:44 +0400

    remove un-needed stuff

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { DataModule } from './data/data.module';
 import { SchedulerModule } from './scheduler/scheduler.module';
 import { UserModule } from './user/user.module';
 
-@Module({
-  imports: [DataModule, SchedulerModule, UserModule],
-  controllers: [AppController],
-  providers: [AppService],
-})
+@Module({ imports: [DataModule, SchedulerModule, UserModule] })
 export class AppModule {}
