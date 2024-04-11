3cda796c869f82bc97816f93e478758249f225c7
alakhpc <36898190+alakhpc@users.noreply.github.com>
2024-04-12 03:44:34 +0400

    format

--- src/scheduler/interval.decorator.ts
+++ src/scheduler/interval.decorator.ts
@@ -1,5 +1,4 @@
 import { SetMetadata } from '@nestjs/common';
 import { SCHEDULER_TOKEN, SchedulerMeta } from './scheduler.consts';
 
-export const Interval = (meta: SchedulerMeta) =>
-  SetMetadata(SCHEDULER_TOKEN, meta);
+export const Interval = (meta: SchedulerMeta) => SetMetadata(SCHEDULER_TOKEN, meta);
